5b3e10e7f4aeac6c581f95a8d0718c2ef9ba127a
Stefan Stefanov <stefan_stefanov@1stonlinesolutions.com>
2015-05-11 18:07:54 +0300

    adding async helpers and AjaxApi blank file

--- frontend/lib/helpers.js
+++ frontend/lib/helpers.js
@@ -1,4 +1,95 @@
 
 module.exports = {
   
-}
+  chain: function(fns, context){
+    var self = this;
+    fns = fns.map(function(f){
+      if(typeof f !== "function") {
+        var that = self;
+        return function(){
+          var ctx = this;
+          var args = Array.prototype.slice.call(arguments);
+          var chain_cb = args.pop();
+          that.amap(f, function(ob_fn, amap_cb){
+            ob_fn.apply(ctx, args.concat([amap_cb]));
+          }, chain_cb);
+        };
+      }
+      else{
+        return f;
+      }
+    });
+
+
+    return function(){
+      var cb, ctx = context || this, ch = fns, ptr = -1;
+      var args = Array.prototype.slice.call(arguments);
+      var cb = args.pop();
+      if(typeof cb !== "function") ctx = cb, cb = args.pop();
+      if(typeof cb !== "function") throw new Error("Got you!!");
+      if(fns.length){
+        ptr++;
+        args.push(next);
+        fns[0].apply(ctx, args);
+      }
+      else{
+        args.unshift(null);
+        cb.apply(ctx, args);
+      }
+      function next(){
+        var err = arguments[0];
+        if(err) return cb(err);
+        if(!ch[++ptr]) return cb.apply(ctx, arguments);
+        try{ch[ptr].apply(ctx, Array.prototype.slice.call(arguments, 1).concat([next]));}
+        catch(err){cb(err);}
+      }
+    }
+  },
+
+  runFnsIterator: function(fn, cb){fn(cb);},
+
+  amapCompose: function(obj, iterator){
+    var self = this;
+    return function(ob, itr, cb){
+      self.amap( ob||obj, itr||iterator, cb);
+    }
+  },
+
+  amap: function(arr, iterator, cb){
+    if(!iterator) iterator = this.runFnsIterator;
+    else if(Array.isArray(iterator)) iterator = this.chain(iterator);
+    var results, counter;
+    if(!Array.isArray(arr)){
+      results = {};
+      counter = Object.keys(arr).length;
+      arr.forEach = function(itr){
+        for(var key in arr) {
+          if(key==="forEach") continue;
+          itr(arr[key], key, arr);
+        }
+      }
+    }
+    else{
+      counter = arr.length;
+      results = new Array(arr.length);
+    }
+    if(counter===0) return cb(null, arr);
+
+    var  error;
+    arr.forEach(function(el, idx, arr){
+      setTimeout(function(){
+        iterator(el, function(err, result){
+          if(error === true) return;
+          if(err){error = true; return cb(err);}
+          results[idx] = result;
+          counter--;
+          if(counter===0) {
+            cb(null, results);
+          }
+        });
+      }, 0);
+    });
+    if(!Array.isArray(arr)) delete arr.forEach;
+  }
+  
+};
